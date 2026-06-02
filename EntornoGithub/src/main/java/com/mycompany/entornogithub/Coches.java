5b22c9566e733b3089076343071b89af9e13f14d
Pablo Miranda <mirandahidalgopablo@gmail.com>
2026-06-02 10:09:06 +0200

    Adicion de sets y metodo
    Se le han añadido los metodos set y un metodo que diga hola

--- EntornoGithub/src/main/java/com/mycompany/entornogithub/Coches.java
+++ EntornoGithub/src/main/java/com/mycompany/entornogithub/Coches.java
@@ -1,24 +1,21 @@
-
 package com.mycompany.entornogithub;
 
 public class Coches {
+
     //atributos como la marca del coche su nombre su motor sus kilometros su año su cilindrada
-    private String marca; 
+    private String marca;
     private double km;
-    
-    
-    
-    public Coches(){
-        String marca="";
+
+    public Coches() {
+        String marca = "";
         double km = 0;
         int año = 0;
         int cv = 0;
         String motor = "";
-        String nombre ="";
+        String nombre = "";
         int capacidad = 0;
-        
+
     }
-    
 
     public Coches(String marca, double km, int año, int cv, String motor, String nombre, int capacidad) {
         this.marca = marca;
@@ -34,4 +31,34 @@ public class Coches {
     private String motor;
     private String nombre;
     private int capacidad;
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public void setCv(int cv) {
+        this.cv = cv;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    
+    public String diHola
 }
