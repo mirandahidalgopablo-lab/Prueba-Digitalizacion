bf8b898d93e7704f8ca0af5210009010e4f837e1
XxSAULxXo7 <sbarram0702@g.educaand.es>
2026-06-02 10:05:36 +0200

    Add files via upload
    Constructores

--- EntornoGithub/src/main/java/com/mycompany/entornogithub/Coches.java
+++ EntornoGithub/src/main/java/com/mycompany/entornogithub/Coches.java
@@ -5,6 +5,30 @@ public class Coches {
     //atributos como la marca del coche su nombre su motor sus kilometros su año su cilindrada
     private String marca; 
     private double km;
+    
+    
+    
+    public Coches(){
+        String marca="";
+        double km = 0;
+        int año = 0;
+        int cv = 0;
+        String motor = "";
+        String nombre ="";
+        int capacidad = 0;
+        
+    }
+    
+
+    public Coches(String marca, double km, int año, int cv, String motor, String nombre, int capacidad) {
+        this.marca = marca;
+        this.km = km;
+        this.año = año;
+        this.cv = cv;
+        this.motor = motor;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+    }
     private int año;
     private int cv;
     private String motor;
